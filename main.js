e6f5790c3e961324db9260b781531afaaf4a2115
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-08 10:07:56 -0400

    Converting the mins and seconds into numbers using parseInt and then i calculated the total number of seconds using reduce and then i got the hours by dividing the seconds left by 3600 becuase that is the number of seconds in an hour and then i added parseInt to that to give me a whole number

--- main.js
+++ main.js
@@ -5,5 +5,12 @@ console.log(time)
 
 const seconds = time.map((t => t.dataset.time)).map((timecode => {
   const [mins, seconds] = timecode.split(":")
-  console.log(mins, seconds)
-}))
+  // console.log(mins, seconds)
+  let minutes = parseInt(mins)
+  let sec = parseInt(seconds)
+  return (minutes * 60) + sec
+})).reduce((total, seconds) => total + seconds)
+
+let secondsLeft = seconds;
+const hours = parseInt(secondsLeft / 3600)
+secondsLeft = 
